6d629c18012f9680b3409b40a4057e40648d8fb2
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-07 11:39:36 +0200

    add: user name acquired and saved in a variable

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,7 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
@@ -9,5 +11,14 @@ public class CheckGuest {
 		
 		//Guests array population
 		partyGuests = new String[] {" Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardis Zarei","Martina Maccherone","Rachel Zeilic"};
+		
+		//Create an instance of the Scanner class and ask the user his name
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Inserisci il tuo nome(nome e cognome o nome d'arte) per verificare se sei nella lista degli invitati: ");
+		
+		//Initialize a variable and assign to it the user input
+		String userName = scanner.nextLine();
+		
+	
 	}
 }
