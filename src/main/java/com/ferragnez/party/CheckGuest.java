7eb104e005fc9d4afe62a0c7c7773940db8a7512
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-07 11:05:50 +0200

    add: partyGuests array declared and populated

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -3,8 +3,11 @@ package com.ferragnez.party;
 public class CheckGuest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Guests array declaration
+		String partyGuests[];
+		
+		//Guests array population
+		partyGuests = new String[] {" Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardis Zarei","Martina Maccherone","Rachel Zeilic"};
 	}
-
 }
